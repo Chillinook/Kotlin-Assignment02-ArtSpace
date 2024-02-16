c8271ca5c0c58563ff332ec660e922dd2e398130
Chillinook <kwanmygirl@gmail.com>
2024-02-16 15:04:34 -0700

    4th finish all task

--- app/src/main/java/com/example/assignment02_artspace/MainActivity.kt
+++ app/src/main/java/com/example/assignment02_artspace/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
+//import androidx.compose.foundation.layout.FlowColumnScopeInstance.weight
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -18,6 +22,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.DividerDefaults.color
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -30,6 +35,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -64,36 +70,6 @@ fun MotoSpaceApp() {
 //    MotoList( CardList = Datasource().loadMoto() ) ;
     MotoSpace( CardList = Datasource().loadMoto() ) ;
 }
-
-@Composable
-fun MotoCard( MotoList: Moto ) {
-    Card(
-        modifier = Modifier
-            .padding(20.dp)
-    ){
-        Image(
-            painter = painterResource(id = MotoList.imageResourceId),
-            contentDescription = stringResource(id = MotoList.riderNameId ),
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-        Text(
-            text = stringResource(id = MotoList.riderNameId ),
-            textAlign = TextAlign.Center
-        )
-    }
-}
-
-@Composable
-fun MotoList( CardList: List<Moto> ) {
-    LazyColumn {
-        items(CardList){
-                MotoList -> MotoCard( MotoList );
-        }
-    }
-}
-
 @Composable
 fun MotoSpace( CardList: List<Moto>){
     var motoNum by remember { mutableStateOf(0) }
@@ -104,48 +80,62 @@ fun MotoSpace( CardList: List<Moto>){
 //            verticalAlignment = Alignment.CenterVertically,
 //            horizontalArrangement = Arrangement.Center,
             modifier = Modifier
-                .fillMaxHeight(0.7f)
+//                .weight(3.8f)
                 .padding(50.dp)
         ){
+//
             Image(
-
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .border(20.dp, Color.White)
+                    .size(width = 340.dp, height = 400.dp),
                 painter = painterResource(id = CardList[motoNum].imageResourceId),
-                contentDescription = stringResource(id = R.string.riderDate5) )
-
-
+                contentDescription = stringResource(id = R.string.riderDate5)
+            )
         }
         Row (
+            modifier = Modifier
+                .padding(20.dp),
+//                .fillMaxSize(),
+//                .weight(1.2f)
+//                .fillMaxWidth(),
+//                .fillMaxHeight(),
             horizontalArrangement = Arrangement.Center,
             verticalAlignment = Alignment.Top,
-            modifier = Modifier
-                .padding(30.dp)
-                .fillMaxWidth(),
+
 
         ){
-            Column(//
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .background(Color(0xFFD6F1F2))
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.60f)
+                    .padding(10.dp)
 
             ) {
-//                Text(
-//                    text = stringResource(id = CardList[motoNum].riderNameId ),
-//                    fontSize = 36.sp
-//                )
-//                Text(
-//                    text = stringResource(id = CardList[motoNum].riderDateId ),
-//                    fontSize = 22.sp
-//                )
+                Text(
+//                    modifier = Modifier
+//                        .padding(10.dp),
+                    text = stringResource(id = CardList[motoNum].riderNameId ),
+                    fontSize = 30.sp
+                )
+                Text(
+                    text = stringResource(id = CardList[motoNum].riderDateId ),
+                    fontSize = 20.sp
+                )
             }
-
         }
         Row (
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround,
+//            verticalAlignment = Alignment.Bottom,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(15.dp)
+                .padding(top = 20.dp)
         ){
             Button(
                 modifier = Modifier
-                    .size(height = 35.dp , width = 180.dp ),
+                    .size(height = 35.dp , width = 140.dp ),
                 onClick = {
                     if (motoNum < CardList.size)
                         motoNum--
@@ -156,14 +146,14 @@ fun MotoSpace( CardList: List<Moto>){
             }
             Button(
                 modifier = Modifier
-                    .size(height = 35.dp , width = 180.dp ),
+                    .size(height = 35.dp , width = 140.dp ),
                 onClick = {
                     if (motoNum < CardList.size)
                         motoNum++
                     if (motoNum == CardList.size )
                         motoNum = 0
-                          },
-                shape = RectangleShape) {
+                          })
+               {
                 Text(text = "Next")
             }
         }
